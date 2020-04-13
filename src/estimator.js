1ae2ce381bb9165d1ef363634bc67f2876d66896
Adekanye Oluwayomi <52767095+kanyeunited@users.noreply.github.com>
2020-04-13 14:13:49 +0100

    my assessment

--- src/estimator.js
+++ src/estimator.js
@@ -10,35 +10,35 @@ const covid19ImpactEstimator = (data) => {
 
   let period = 0;
 
-  if (data.periodType === 'days')  period = data.timeToElapse;
-    else if (data.periodType === 'weeks') period = data.timeToElapse * 7;
-    else if (data.periodType === 'months') period = data.timeToElapse * 30;
+if (data.periodType === 'days')  period = data.timeToElapse;
+  else if (data.periodType === 'weeks') period = data.timeToElapse * 7;
+  else if (data.periodType === 'months') period = data.timeToElapse * 30;
 
   const infectionsByRequestedTime = () => {
     return this.currentlyInfected * (2 ** Number(period / 3));
-  }
+  };
 
   const severeCasesByRequestedTime = () => {
     return Number(this.infectionsByRequestedTime * 0.15);
-  }
+  };
 
   const hospitalBedsByRequestedTime = () => {
     return (data.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime;
-  }
+  };
 
   const casesForICUByRequestedTime = () => {
     return Number(this.infectionsByRequestedTime * 0.05);
-  }
+  };
 
   const casesForVentilatorsByRequestedTime = () => {
     return Number(this.infectionsByRequestedTime * 0.02);
-  }
+  };
 
   const dollarsInFlight = () => {
     return Number((infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD) / period);
-  }
+  };
 
-  const impact = {
+  const impact = (data) => {
     currentlyInfected: data.reportedCases * 10,
     infectionsByRequestedTime: infectionsByRequestedTime,
     severeCasesByRequestedTime: severeCasesByRequestedTime,
@@ -46,9 +46,9 @@ const covid19ImpactEstimator = (data) => {
     casesForICUByRequestedTime: casesForICUByRequestedTime,
     casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime,
     dollarsInFlight: dollarsInFlight
-  }
+  };
 
-  const severeImpact = {
+  const severeImpact = (data) => {
     currentlyInfected: data.reportedCases * 50,
     infectionsByRequestedTime: infectionsByRequestedTime,
     severeCasesByRequestedTime: severeCasesByRequestedTime,
@@ -56,13 +56,13 @@ const covid19ImpactEstimator = (data) => {
     casesForICUByRequestedTime: casesForICUByRequestedTime,
     casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime,
     dollarsInFlight: dollarsInFlight
-  }
+  };
 
   const estimate = {
     estimate: {
       impact: impact,
       severeImpact: severeImpact
-    }
+    };
   }
 }
 
