693557688ea2fa048a23cfe0d5d80b6768e4ddf4
Adekanye Oluwayomi <52767095+kanyeunited@users.noreply.github.com>
2020-04-13 13:46:17 +0100

    my assessment

--- src/estimator.js
+++ src/estimator.js
@@ -10,32 +10,57 @@ const covid19ImpactEstimator = (data) => {
 
   let period = 0;
 
-  if (data.periodType === 'days') {
-      period = data.timeToElapse;
-    }else if (data.periodType === 'weeks') {
-      period = data.timeToElapse * 7;
-    }else if (data.periodType === 'months') {
-      period = data.timeToElapse * 30;
-    }
+  if (data.periodType === 'days')  period = data.timeToElapse;
+    else if (data.periodType === 'weeks') period = data.timeToElapse * 7;
+    else if (data.periodType === 'months') period = data.timeToElapse * 30;
 
-  const impact = (data) => {
-    const currentlyInfected = data.reportedCases * 10;
-    const infectionsByRequestedTime = currentlyInfected * (2 ** Number(period / 3));
-    const severeCasesByRequestedTime =  Number(infectionsByRequestedTime * 0.15);
-    const hospitalBedsByRequestedTime = (data.totalHospitalBeds * 0.35) - severeCasesByRequestedTime;
-    const casesForICUByRequestedTime = Number(infectionsByRequestedTime * 0.05);
-    const casesForVentilatorsByRequestedTime = Number(infectionsByRequestedTime * 0.02);
-    const dollarsInFlight = Number((infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD) / period);
+  const infectionsByRequestedTime = () => {
+    return this.currentlyInfected * (2 ** Number(period / 3));
   }
 
-  const severeImpact = (data) => {
-    const currentlyInfected = data.reportedCases * 50;
-    const infectionsByRequestedTime = currentlyInfected * (2 ^ Number(period / 3));
-    const severeCasesByRequestedTime =  Number(infectionsByRequestedTime * 0.15);
-    const hospitalBedsByRequestedTime = (data.totalHospitalBeds * 0.35) - severeCasesByRequestedTime;
-    const casesForICUByRequestedTime = Number(infectionsByRequestedTime * 0.05);
-    const casesForVentilatorsByRequestedTime = Number(infectionsByRequestedTime * 0.02);
-    const dollarsInFlight = Number((infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD) / period);
+  const severeCasesByRequestedTime = () => {
+    return Number(this.infectionsByRequestedTime * 0.15);
+  }
+
+  const hospitalBedsByRequestedTime = () => {
+    return (data.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime;
+  }
+
+  const casesForICUByRequestedTime = () => {
+    return Number(this.infectionsByRequestedTime * 0.05);
+  }
+
+  const casesForVentilatorsByRequestedTime = () => {
+    return Number(this.infectionsByRequestedTime * 0.02);
+  }
+
+  const dollarsInFlight = () => {
+    return Number((infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD) / period);
+  }
+
+  const impact = {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: infectionsByRequestedTime,
+    severeCasesByRequestedTime: severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime: hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime: casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime,
+    dollarsInFlight: dollarsInFlight
+  }
+
+  const severeImpact = {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: infectionsByRequestedTime,
+    severeCasesByRequestedTime: severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime: hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime: casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime,
+    dollarsInFlight: dollarsInFlight
+  }
+
+  export const estimate = {
+    impact: impact,
+    severeImpact: severeImpact
   }
 }
 
