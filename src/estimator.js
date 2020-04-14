04164c20e11d01319c5ebd3f20d5f042c4aa6120
Adekanye Oluwayomi <52767095+kanyeunited@users.noreply.github.com>
2020-04-14 06:30:54 +0100

    my assessment

--- src/estimator.js
+++ src/estimator.js
@@ -3,9 +3,9 @@ let period = 0;
 const currentlyInfected = (data, estimate) => data.reportedCases * estimate;
 
 const infectionsByRequestedTime = (data, estimate) => {
-  if (data.periodType.trim() === 'days') period = data.timeToElapse;
-  else if (data.periodType.trim() === 'weeks') period = data.timeToElapse * 7;
-  else if (data.periodType.trim() === 'months') period = data.timeToElapse * 30;
+  if (data.periodType.toLowerCase() === 'days') period = data.timeToElapse;
+  else if (data.periodType.toLowerCase() === 'weeks') period = data.timeToElapse * 7;
+  else if (data.periodType.trimtoLowerCase() === 'months') period = data.timeToElapse * 30;
 
   return currentlyInfected(data, estimate) * (2 ** Math.trunc(period / 3));
 };
@@ -34,16 +34,16 @@ const hospitalBedsByRequestedTime = (data, estimate) => {
 const covid19ImpactEstimator = (data) => ({
   data,
   estimate: {
-    impact: {
-      currentlyInfected: any currentlyInfected(data, 10),
-      infectionsByRequestedTime: any infectionsByRequestedTime(data, 10),
+    impact: any {
+      currentlyInfected: currentlyInfected(data, 10),
+      infectionsByRequestedTime: infectionsByRequestedTime(data, 10),
       severeCasesByRequestedTime: Math.trunc(infectionsByRequestedTime(data, 10) * 0.15),
       hospitalBedsByRequestedTime: hospitalBedsByRequestedTime(data, 10),
       casesForICUByRequestedTime: Math.trunc(infectionsByRequestedTime(data, 10) * 0.05),
       casesForVentilatorsByRequestedTime: Math.trunc(infectionsByRequestedTime(data, 10) * 0.02),
       dollarsInFlight: dollarsInFlight(data, 10)
     },
-    severeImpact: {
+    severeImpact: any {
       currentlyInfected: currentlyInfected(data, 50),
       infectionsByRequestedTime: infectionsByRequestedTime(data, 50),
       severeCasesByRequestedTime: Math.trunc(infectionsByRequestedTime(data, 50) * 0.15),
