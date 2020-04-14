cdf7c7ef920f22c3f0c45d119085e82727597c83
Adekanye Oluwayomi <52767095+kanyeunited@users.noreply.github.com>
2020-04-14 20:52:07 +0100

    my assessment

--- src/estimator.js
+++ src/estimator.js
@@ -2,9 +2,9 @@ const covid19ImpactEstimator = (data) => {
   const { reportedCases, periodType, timeToElapse } = data;
 
   let period = 0;
-  if (periodType === "days") period = timeToElapse;
-  else if (periodType === "weeks") period = timeToElapse * 7;
-  else if (periodType === "months") period = timeToElapse * 30;
+  if (periodType === 'days') period = timeToElapse;
+  else if (periodType === 'weeks') period = timeToElapse * 7;
+  else if (periodType === 'months') period = timeToElapse * 30;
 
   const currentlyInfected = (estimate) => reportedCases * estimate;
 
@@ -34,7 +34,7 @@ const covid19ImpactEstimator = (data) => {
     data,
     estimate: {
       impact: {
-        currentlyInfected: currentlyInfected(10),
+        currentlyInfected: timeToElapse * 10,
         infectionsByRequestedTime: infectionsByRequestedTime(10),
         severeCasesByRequestedTime: Math.trunc(infectionsByRequestedTime(10) * 0.15),
         hospitalBedsByRequestedTime: hospitalBedsByRequestedTime(10),
